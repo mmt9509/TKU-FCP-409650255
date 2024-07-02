6442d400dee47bf0e73e3933de243191c38257de
mmt9509 <mmt9509@gmail.com>
2024-07-02 11:50:25 +0800

    我的第一支java程式

---
+++ CH2_1.java
@@ -0,0 +1,13 @@
+// CH2_1, 間單的Java程式
+public class CH2_1{   //定義public類別CH2_1
+    public static void main(String[] args) {    //main()函數，主程式開始
+        int num1=2;    //宣告函數num
+        int num2=3;    //宣告函數num
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+" cats are running");  //印出字串及變數內容
+        System.out.println(num2+" dogs are running");  //印出字串及變數內容
+        System.out.println(num3+" birds are running");  //印出字串及變數內容
+        System.out.println(num4+" tigers are running");  //印出字串及變數內容
+    }
+}
