ee3b5847a2daeeaea0924dd62bcd255ad2ae4f04
mmt9509 <mmt9509@gmail.com>
2024-07-03 10:55:21 +0800

    增加CH2_2,CH2_3

--- CH2_1.java
+++ CH2_1.java
@@ -1,8 +1,8 @@
-// CH2_1, 間單的Java程式
 public class CH2_1{   //定義public類別CH2_1
     public static void main(String[] args) {    //main()函數，主程式開始
-        int num1=2;    //宣告函數num
-        int num2=3;    //宣告函數num
+        int num1,num2;  //宣告函數num1,num2
+        num1=2;    //給值
+        num2=3;    //給值
         float num3=4;
         String num4="五";
         System.out.println(num1+" cats are running");  //印出字串及變數內容
---
+++ Ch2_3.java
@@ -0,0 +1,6 @@
+public class Ch2_3 {   //定義類別Ch2_3
+    public static void main(String[] args) {
+        int a=2,b=5;   //宣告變數a和b
+        System.out.println(a+"*"+b+"="+(a*b));  //印出運算結果
+    }
+}
