53ae29eb3434fe58e73c0e5f586548b0ecba530d
mmt9509 <mmt9509@gmail.com>
2024-07-09 11:59:27 +0800

    CH4

---
+++ CH4_6.java
@@ -0,0 +1,11 @@
+public class CH4_6 {
+    public static void main(String[] args) {
+        int a=7,b=13;
+        System.out.printf("%d & %d=%d\n", a,b,a&b);
+        System.out.printf("%d | %d=%d\n", a,b,a|b);
+        System.out.printf("!%d=%d\n", a,~a);
+        System.out.printf("%d ^ %d=%d\n", a,b,a^b);
+        System.out.printf("%d >> 2=%d\n", a,a>>2);
+        System.out.printf("%d << 2=%d\n", a,a<<2);
+    }
+}
