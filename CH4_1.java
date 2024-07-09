b16352d72c16be48cdd940aa6336cc4bfa9a9bbf
mmt9509 <mmt9509@gmail.com>
2024-07-09 11:25:38 +0800

    CH4

---
+++ CH4_1.java
@@ -0,0 +1,17 @@
+public class CH4_1 {
+    public static void main(String[] args) {
+        int a=9,b=5;
+    
+        System.out.printf("%d + %d=%d\n",a,b,a+b);
+        System.out.printf("%d - %d=%d\n",a,b,a-b);
+        System.out.printf("%d * %d=%d\n",a,b,a*b);
+        System.out.printf("%d / %d=%d\n",a,b,a/b);
+        System.out.printf("%d %% %d=%d\n",a,b,a%b);
+    
+    
+    
+    
+    
+        
+    }    
+}
