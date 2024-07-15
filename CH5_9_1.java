bd9b2215ecdc70ec4e368d7d1c7e1be16d9eb36d
mmt9509 <mmt9509@gmail.com>
2024-07-15 11:44:58 +0800

    CH5

---
+++ CH5_9_1.java
@@ -0,0 +1,9 @@
+public class CH5_9_1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=12;j++)
+              System.out.printf("%d*%d=%2d  ", i,j,i*j);
+            System.out.println();  
+        }
+    }
+}
